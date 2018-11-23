701dd8af2fea0eaf7f671ac677dfe10c5b696ee1
Eugene Hong <doluseventualis@gmail.com>
2018-11-23 10:21:13 +0900

    fixed?

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    artist: '프라푸치노 맛있다'
-  });
+  res.render('index');
 });
+
 
 module.exports = router;
